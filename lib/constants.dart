cbb5be8edfbbf5e378f1ebfce7346a7a0a9745f6
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2023-08-24 17:55:33 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -12,7 +12,6 @@ const kMessageTextFieldDecoration = InputDecoration(
   border: InputBorder.none,
 );
 
-
 const kMessageContainerDecoration = BoxDecoration(
   border: Border(
     top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
